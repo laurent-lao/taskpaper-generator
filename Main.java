b473d0ab98a301c41102a85a05223b5e69a780bb
laurentlaurent <lao.laurent@gmail.com>
2019-02-21 13:47:11 -0500

    Added simplified Chapter listing

--- Main.java
+++ Main.java
@@ -27,8 +27,9 @@ public class Main {
     public static void displayMenu()
 	{
 		System.out.print("Select a template" + "\n" +
-				"1. Creating Chapter Tasks\n" +
-				"" +
+				"1. Create Chapter Tasks (simple)\n" +
+				"2. Create Chapter Tasks (extended)\n" +
+				"" + // add more options here
 				"\n\tInput option> ");
 	}
 
@@ -36,7 +37,10 @@ public class Main {
 	{
 		switch (choice) {
 			case 1:
-				createChapterTasks();
+				createChapterTasks_simple();
+				return true;
+			case 2:
+				createChapterTasks_extended();
 				return true;
 			default:
 				System.out.print("Wrong choice, try again > ");
@@ -44,9 +48,74 @@ public class Main {
 			}
 	}
 
-	// Creates a list of tasks for the chapters to come
-	public static void createChapterTasks()
+	public static void createChapterTasks_simple() {
+		// Instructions
+		System.out.println("You can build a list of chapters in Excel/Numbers.\n" +
+				"Start with the Chapter name, then list all subchapters, end the list with \"stop\", and copy one more row (to act as a escape).");
+
+    	//	Prompt user for the amount of Chapters
+		System.out.print("How many chapters? > ");
+		int howManyChapters = keyIn.nextInt();
+
+		//	Creates the 2-d array that will store the tasks
+		Task[][] chapterTasks = new Task[howManyChapters][100];
+
+		//	Prompt user for the prefix
+		System.out.print("Whats the name of the class? (or just the book name) If there are multiple textbooks, input class + name here. \ni.e. BIOL 206 LAB BOOK > ");
+		String nameOfClass = keyInString.nextLine();
+		Task.setTaskNamePrefix((nameOfClass + " Chapter"));
+
+		//	Create a project for the chapters
+		Task project = new Task(0, (nameOfClass));
+		project.setTaskIsParallel(false);
+		project.setTaskIsAutodone(true);
+
+		// Prompt user for the information about the Chapters
+		for (int i = 0; i < howManyChapters; i++)
+		{
+			// Prompt the user for the Title of the Chapter
+			System.out.print("What's the title of the Chapter " + (i + 1) + "? > ");
+			String chapterTitle = keyInString.nextLine();
+			chapterTasks[i][0] = new Task(1, "Read", ((i + 1) + ": " + chapterTitle));
+			chapterTasks[i][0].setTaskIsParallel(false);
+			chapterTasks[i][0].setTaskIsAutodone(true);
+
+			int subChapterCounter = 1;
+			int arrayAt           = 0;
+
+			for (int j = 1; j < 100; j++, subChapterCounter++, arrayAt = j)
+			{
+				// Prompt the user for the Title of the Sub Chapter
+				System.out.print("What's the title of the sub-Chapter #" + (i + 1) + "." + subChapterCounter + "?" +
+						"\n(Enter stop to go to the next Chapter) > ");
+				String subchapterTitle = keyInString.nextLine();
+				if (!subchapterTitle.equalsIgnoreCase("stop"))
+				{
+					chapterTasks[i][j] = new Task(2, "Read", ((i + 1) + "." + subChapterCounter + ": " + subchapterTitle));
+				}
+				else
+					break;
+
+			} // End of nested for loop
+		} // End of first for loop
+
+		// Print the tasks
+		createChapterTasks_printing(project, chapterTasks, howManyChapters);
+	} // End of method
+
+	// Creates a list of tasks for the chapters to come with extended actions
+	// i.e
+	// Study: SOEN 228 Textbook Chapter 1: Basic Structure of Computers @parallel(false) @autodone(false)
+	//			- Study: SOEN 228 Textbook Chapter 1.1: Computer Types @parallel(false) @autodone(false)
+	//				- Read: SOEN 228 Textbook Chapter 1.1: Computer Types @parallel(true) @autodone(false)
+	//				- Annotate: SOEN 228 Textbook Chapter 1.1: Computer Types @parallel(true) @autodone(false)
+	//				- Analyze Annotation: SOEN 228 Textbook Chapter 1.1: Computer Types @parallel(true) @autodone(false)
+	public static void createChapterTasks_extended()
 	{
+		// Instructions
+		System.out.println("You can build a list of chapters in Excel/Numbers.\n" +
+				"Start with the Chapter name, then list all subchapters, end the list with \"stop\", and copy one more row (to act as a escape).");
+
 		//	Prompt user for the amount of Chapters
 		System.out.print("How many chapters? > ");
 		int howManyChapters = keyIn.nextInt();
@@ -55,13 +124,14 @@ public class Main {
 		Task [][] chapterTasks = new Task[howManyChapters][100];
 
 		//	Prompt user for the prefix
-		System.out.print("Whats the name of the class? If there are multiple textbooks, input here.\ni.e. BIOL 206 LAB BOOK > ");
+		System.out.print("Whats the name of the class or book? If there are multiple textbooks, input Class + name here.\ni.e. BIOL 206 LAB BOOK > ");
 		String nameOfClass = keyInString.nextLine();
 		Task.setTaskNamePrefix((nameOfClass + " Chapter"));
 
 		//	Create a project for the chapters
 		Task project = new Task(0,(nameOfClass));
 		project.setTaskIsParallel(false);
+		project.setTaskIsAutodone(true);
 
 		// Prompt user for the information about the Chapters
 		for (int i = 0; i < howManyChapters; i++)
@@ -105,6 +175,12 @@ public class Main {
 
 		}
 
+		// Print the tasks
+		createChapterTasks_printing(project, chapterTasks, howManyChapters);
+	}
+
+	public static void createChapterTasks_printing(Task project, Task [][] chapterArray, int howManyChapters)
+	{
 		// Printing all the Chapters info
 		System.out.println("===== START OF TASKPAPER =====\n\n");
 
@@ -114,13 +190,13 @@ public class Main {
 		// Printing chapters
 		for (int i = 0; i < howManyChapters; i++)
 		{
-			System.out.println(chapterTasks[i][0]);
+			System.out.println(chapterArray[i][0]);
 
 			for (int j = 1; j < 100; j++)
 			{
-				if (chapterTasks[i][j] != null)
+				if (chapterArray[i][j] != null)
 				{
-					System.out.println(chapterTasks[i][j]);
+					System.out.println(chapterArray[i][j]);
 				}
 				else
 					break;
